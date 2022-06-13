e8f57cb0d8e6805de9974c70d3342571a2a019bb
Spoon2 <viktor.spoon2@gmail.com>
2022-06-13 20:44:07 +0400

    Изменил название модуля

---
+++ data_input.py
@@ -0,0 +1,32 @@
+import csv
+from encodings import utf_8
+
+def name_input():
+    symbols = "!'@#$%^&*()`/:;.,?<>\|'""+ -=[]}{"
+    obj = input('Введите ФИО абонента в формате Иванов Иван Иванович: ')
+    while True:
+        for char in obj:
+            if char.isdigit() or char.isalnum() or char in symbols:
+                return obj
+            else:
+                print('Вы ввели недопустимый символ, попробуйте снова')    
+
+def number_input():
+    obj = input('Введите номер абонента в формате 8800123: ')
+    while True: 
+        for char in obj:
+            if char.isdigit():
+                return obj
+            else:
+                print('Вы ввели недопустимый символ, попробуйте снова ')    
+
+def library_fill(card): #card - карточка абонента
+        with open('library.csv', 'a', encoding='utf_8') as lib:
+            csv.register_dialect('my_dialect', delimiter='|', lineterminator="\r\n")
+            writer = csv.writer(lib, 'my_dialect')
+            writer.writerow(card)
+
+def card_creator():
+    card = input("Введите данные строго в формате -> Фамилия Имя Отчество Номер телефона Тип номера: ")
+    card = card.split()
+    return card
