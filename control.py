714ccb8273574760a7a8117343c31f46dc5f01e0
Spoon2 <viktor.spoon2@gmail.com>
2022-06-13 23:11:59 +0400

    Обновил ввод и интерфейс

--- control.py
+++ control.py
@@ -1,43 +1,53 @@
+import data_input
 
 def function_select():
     func_select = input('Выберите режим работы:\
         1. Поиск абонента или номера телефона\
         2. Редактор справочника\
-        3. Выход ')
-    while not func_select.isdigit():            #Пробовал использовать рекурсию, не получилось
+        3. Выход ')    
+    if not func_select.isdigit() and int(func_select)<1 or int(func_select)>3: 
         print('Вы ввели недопустимое значение, попробуйте снова ')
-        func_select = input('Выберите режим работы:\
-            1. Поиск абонента или номера телефона\
-            2. Редактор справочника\
-            3. Выход ')
-    print()
+        func_select = function_select()       
+    else:
+        print()
     return int(func_select)
-
 
 def search_select():
     srch_select = input('Выберите режим поиска:\
         1. Поиск по имени\
         2. Поиск по номеру телефона\
         3. Назад ')
-    while not srch_select.isdigit():
+    if not srch_select.isdigit() and int(srch_select)<1 or int(srch_select)>3:
         print('Вы ввели недопустимое значение, попробуйте снова ')
-        srch_select = input('Выберите режим поиска:\
-            1. Поиск по имени\
-            2. Поиск по номеру телефона\
-            3. Назад ')
+        srch_select = search_select()
     print()
     return int(srch_select)
+
 
 def get_card():
     get_data = input('Найти номер телефона или карточку абонента?\
         1. Номер телефона\
         2. Карточку абонента\
         3. Назад ')
-    while not get_data.isdigit():
+    if not get_data.isdigit() and int(get_data)<1 or int(get_data)>3:
         print('Вы ввели недопустимое значение, попробуйте снова ')
-        get_data = input('Найти номер телефона или карточку абонента?\
-        1. Номер телефона\
-        2. Карточку абонента\
-        3. Назад ')
+        get_data = get_card()
     print()
     return int(get_data)
+
+
+def new_card():
+    card = data_input.card_creator()
+    data_input.library_fill(card)
+    return card
+
+
+def library_redact():
+    redact = input('Выберите действие: \
+        1. Создать новую карточку\
+        2. Назад ')
+    if not redact.isdigit() and int(redact)<1 or int(redact)>2:
+        print('Вы ввели недопустимое значение, попробуйте снова ')
+        redact = library_redact()
+    print()
+    return int(redact)
--- interface.py
+++ interface.py
@@ -18,23 +18,38 @@ while power_on:
                         print('В процессе ')
                         search_select = control.search_select()
                     if search_select == 3:
-                        get_data = control.get_card()
+                        break     
                     else: 
                         print('Вы ввели недопустимое значение, попробуйте снова!')
                         search_select = control.search_select()
             if get_data == 2:
                 while True:
                     search_select = control.search_select()
+                    if search_select == 1:
+                        print('В процессе ')
+                        search_select = control.search_select()
+                    if search_select == 2:
+                        print('В процессе ')
+                        search_select = control.search_select()
+                    if search_select == 3:
+                        break       
             if get_data == 3:
                 func_select = control.function_select()          
     if func_select == 2:
-            print('В процессе')
-            func_select = control.function_select()
+            while True:
+                print('Предупреждение! Будьте внимательны при работе в данном разделе! ')
+                redact = control.library_redact()
+                if redact == 1:
+                    card = control.new_card()
+                    print(f'Новая карточка {card} добавлена в справочник ')
+                if redact == 2:
+                    func_select = control.function_select()
+                    break
+                else: 
+                        print('Вы ввели недопустимое значение, попробуйте снова!')
+                        redact = control.library_redact()
     if func_select == 3:
             print('Спасибо, что пользуетесь нашим приложением. До свидания ')
             print()
             power_on = False
-    else:
-        print('Вы ввели недопустимое значение, попробуйте снова ')
-        func_select = control.function_select()
-
+ 
